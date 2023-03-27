039ac2b4051ce95229f1aa4e4370fbe5153c0bca
user-tania <usertania@gmail.com>
2023-03-27 19:01:42 +0600

    router

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Individual from "./Pages/Individual";
+import Edit from "./Pages/Edit";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import Blog from "./Pages/Blog";
+import Contact from "./Pages/Contact";
+
+
+import "./index.css";
+
+const Layout =()=>{
+  return(
+    <>
+    <Navbar/>
+    <Outlet/>
+    <Footer/>
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children:[
+      {
+        path: "/",
+        element: <div><Home/></div>,
+      },
+      {
+        path: "/blog",
+        element: <div><Blog/></div>,
+      },
+      {
+        path: "/contact",
+        element: <div><Contact/></div>,
+      },
+      {
+        path: "/post/:id",
+        element: <div><Edit/></div>,
+      },
+    ]
+
+  },
+  {
+    path: "/Register",
+    element: <div>Register</div>,
+  },
+  {
+    path: "/login",
+    element: <div>Login</div>,
+  },
+
+]);
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-red-500 underline text-center">
+      <div className="">
+        <RouterProvider router={router} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
+
